8b7b5a0082b48cb39ae39d20b87ecd85e6187623
karankumar945 <innocentboy12021@gmail.com>
2025-12-21 13:10:48 +0530

    Try

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 http.createServer((req,res) =>{
-    res.end("Everything fine")
+    if(req.url === '/'){
+         fs.readFile('index.html','utf8',(err,data)=>{
+        if(err){
+            res.writeHead(501,{'content-type' : 'text/plain'});
+            res.end("Server not found");
+        }
+        else{
+            res.writeHead(501,{'content-type' : 'text/html'});
+            res.end(data);
+        }
+    })
+    };
 }).listen(2000)
 console.log("Server is running at http://localhost:2000/");
